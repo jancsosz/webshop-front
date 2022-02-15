cf7add37a61af5f663ce76485cf54fe96c6f6650
Janos Geszteredi <geszteredi.janika98@gmail.com>
2022-02-15 01:12:10 +0100

    created product.service for individual products

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from './base.service';
+import {HttpClient} from '@angular/common/http';
+import {ProductDto} from '../model/dto/ProductDto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService extends BaseService{
+
+  private productUrl = '/products';
+  private addProductUrl = '';
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  public async getProductGET(id: string): Promise<ProductDto> {
+    const url = `${this.productUrl}/${id}`;
+    const options = {
+      responseType: 'json' as const
+    };
+
+    return (await this.restCall('GET', url, options));
+  }
+
+  public async getProductsGET(): Promise<ProductDto[]> {
+    const options = {
+      responseType: 'json' as const
+    };
+
+    return (await this.restCall('GET', this.productUrl, options));
+  }
+
+  public async addProductPOST(product: ProductDto): Promise<void> {
+    const options = {
+      body: product,
+      responseType: 'json' as const
+    };
+    return (await this.restCall('POST', this.productUrl, options));
+  }
+
+  public async updateProductPUT(product: ProductDto): Promise<void> {
+    const options = {
+      body: product,
+      responseType: 'json' as const
+    };
+
+    return (await this.restCall('PUT', this.productUrl, options));
+  }
+
+  public async deleteProductDELETE(productId: string): Promise<void> {
+    const url = `${this.productUrl}/${productId}`;
+    const options = {
+      responseType: 'json' as const
+    };
+
+    return (await this.restCall('DELETE', url, options));
+  }
+}
