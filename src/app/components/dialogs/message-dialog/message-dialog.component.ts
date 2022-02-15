726d07d21b6805c304870081920afd92fb26c7f1
Janos Geszteredi <geszteredi.janika98@gmail.com>
2022-02-15 01:10:19 +0100

    created message-dialog for client notifications

---
+++ src/app/components/dialogs/message-dialog/message-dialog.component.ts
@@ -0,0 +1,19 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {ProductDto} from '../../../model/dto/ProductDto';
+
+@Component({
+  selector: 'app-message-dialog',
+  templateUrl: './message-dialog.component.html',
+  styleUrls: ['./message-dialog.component.scss']
+})
+export class MessageDialogComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: string
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
