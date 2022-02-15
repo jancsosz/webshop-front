bf4b0d9ecb131b3b2fd6ff040f306a657a8f82d9
Janos Geszteredi <geszteredi.janika98@gmail.com>
2022-02-15 01:07:54 +0100

    modified imports, providers declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,55 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ManageProductsComponent } from './components/manage-products/manage-products.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import { ModifyProductDialogComponent } from './components/dialogs/modify-product-dialog/modify-product-dialog.component';
+import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatOptionModule} from '@angular/material/core';
+import {ReactiveFormsModule} from '@angular/forms';
+import { MessageDialogComponent } from './components/dialogs/message-dialog/message-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    AddProductComponent,
+    ManageProductsComponent,
+    ProductComponent,
+    ProductsComponent,
+    ModifyProductDialogComponent,
+    MessageDialogComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    BrowserAnimationsModule
+    imports: [
+        HttpClientModule,
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule,
+        BrowserAnimationsModule,
+        MatGridListModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatInputModule,
+        MatSelectModule,
+        MatOptionModule,
+        ReactiveFormsModule
+    ],
+  providers: [
+    {
+      provide: MatDialogRef,
+      useValue: {}
+    }
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
