a943c7c9a06ad29af809c9a57c72fba82c099eb6
Janos Geszteredi <geszteredi.janika98@gmail.com>
2022-02-15 01:08:28 +0100

    extended routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {AddProductComponent} from './components/add-product/add-product.component';
+import {ManageProductsComponent} from './components/manage-products/manage-products.component';
+import {ProductsComponent} from './components/products/products.component';
+import {ProductComponent} from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductComponent },
+  { path: 'manage-products', component: ManageProductsComponent },
+  { path: 'add-product', component: AddProductComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
