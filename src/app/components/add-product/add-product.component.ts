d306254078a7859e3dae888cc18437f2052c8e73
Janos Geszteredi <geszteredi.janika98@gmail.com>
2022-03-23 00:21:53 +0100

    created add-product component for product adding (admin function)

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../../services/product.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ProductDto} from '../../model/dto/ProductDto';
+import {MatDialog} from '@angular/material/dialog';
+import {MessageDialogComponent} from '../dialogs/message-dialog/message-dialog.component';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-product',
@@ -7,9 +13,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProductComponent implements OnInit {
 
-  constructor() { }
+  addProductForm: FormGroup;
+  selected: any;
+  isLoading = false;
 
-  ngOnInit(): void {
+  public options = [
+    'FITNESS',
+    'DECORATIONS',
+    'FOOD_SUPPLEMENTS',
+    'GASTRONOMY',
+    'HAIRDRESSING_SUPPLIES',
+    'HOUSEHOLD_SUPPLIES',
+    'LEISURE',
+    'OTHER'
+  ];
+
+  constructor(
+    private productService: ProductService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    public dialog: MatDialog
+  ) {
   }
 
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  public addProduct(): void {
+    if (this.addProductForm.valid && !this.isLoading) {
+      this.isLoading = true;
+      this.productService.addProductPOST(this.createProductDto())
+        .then(() => {
+          this.dialog.open(MessageDialogComponent, {data: 'Product successfully added.'})
+            .afterClosed().subscribe(() => {
+              this.router.navigate(['manage-products']);
+          });
+        })
+        .catch(() => {
+          this.dialog.open(MessageDialogComponent, {data: 'There was an error while creating this product.'});
+          this.isLoading = false;
+        });
+    }
+  }
+
+
+  private createForm(): void {
+    this.addProductForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      price: ['', Validators.required],
+      category: ['', Validators.required],
+      quantityInStock: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+  }
+
+  private createProductDto(): ProductDto {
+    return {
+      name: this.addProductForm.get('name').value,
+      price: this.addProductForm.get('price').value,
+      quantityInStock: this.addProductForm.get('quantityInStock').value,
+      category: this.addProductForm.get('category').value,
+      description: this.addProductForm.get('description').value
+    } as ProductDto;
+  }
 }
