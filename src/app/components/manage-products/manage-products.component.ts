1fffa62b1e4f3562b4eadf6019d526a5edaa4262
Janos Geszteredi <geszteredi.janika98@gmail.com>
2022-03-23 00:34:33 +0100

    created grid layout for products (probably will be moved to products.component)

--- src/app/components/manage-products/manage-products.component.ts
+++ src/app/components/manage-products/manage-products.component.ts
@@ -2,6 +2,8 @@ import {Component, HostListener, OnInit} from '@angular/core';
 import {ProductService} from '../../services/product.service';
 import {ProductDto} from '../../model/dto/ProductDto';
 import {Router} from '@angular/router';
+import {MatDialog} from '@angular/material/dialog';
+import {MessageDialogComponent} from '../dialogs/message-dialog/message-dialog.component';
 
 @Component({
   selector: 'app-manage-products',
@@ -13,18 +15,27 @@ export class ManageProductsComponent implements OnInit {
   products: ProductDto[];
   innerWidth: number;
   colCount: number;
+  isLoading = true;
 
   constructor(
     private productService: ProductService,
-    private router: Router
+    private router: Router,
+    public dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
     this.innerWidth = window.innerWidth;
     this.innerWidth > 768 ? this.colCount = 3 : this.innerWidth > 500 ? this.colCount = 2 : this.colCount = 1;
-    this.productService.getProductsGET().then(result => {
-      this.products = result;
-    });
+    this.productService.getProductsGET()
+      .then(result => {
+        this.products = result;
+        this.products = this.products.concat(result).concat(result).concat(result).concat(result).concat(result).concat(result).concat(result).concat(result).concat(result).concat(result).concat(result).concat(result).concat(result);
+        this.isLoading = false;
+      })
+      .catch((error) => {
+        this.dialog.open(MessageDialogComponent, {data: 'There was an error while loading products.'});
+        this.isLoading = false;
+      });
   }
 
   navigateToProductDetail(path: string): void {
