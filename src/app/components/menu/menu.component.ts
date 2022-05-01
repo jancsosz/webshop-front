036531d1f460586a51f3fd82fd31122d37999e1e
Janos Geszteredi <geszteredi.janika98@gmail.com>
2022-05-01 02:04:25 +0200

    added cart icon to menu (when in /products)

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {environment} from '../../../environments/environment';
+import {MatDialog} from '@angular/material/dialog';
+import {CartDialogComponent} from '../dialogs/cart-dialog/cart-dialog.component';
+import {CartOperationService} from '../../services/cart-operation.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,17 +12,24 @@ import {environment} from '../../../environments/environment';
 })
 export class MenuComponent implements OnInit {
 
+  currentRoute: string;
   loginUrl = `${environment.rootUrl}/login`;
 
   constructor(
-    private router: Router
+    private cartOperationService: CartOperationService,
+    private router: Router,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
+    this.currentRoute = this.router.url;
   }
 
   navigateTo(path: string): void {
     this.router.navigate([path]);
   }
 
+  openCartDialog(): void {
+    this.dialog.open(CartDialogComponent, {data: this.cartOperationService.getCart()});
+  }
 }
