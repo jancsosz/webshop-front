df5240fde26eee9da0ccaba74bd859fa0cdf0b56
Janos Geszteredi <geszteredi.janika98@gmail.com>
2022-05-02 11:35:11 +0200

    navigate away from checkout when cart gets/is empty

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CartOperationService} from '../../services/cart-operation.service';
 import {CartDto} from '../../model/dto/CartDto';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
@@ -12,14 +13,23 @@ export class CheckoutComponent implements OnInit {
   cartDto: CartDto;
 
   constructor(
-    private cartService: CartOperationService
+    private cartService: CartOperationService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.cartDto = this.cartService.getCart();
+    this.navigateToProductsIfCartIsEmpty();
   }
 
   deleteCartItem(cartItemNo: number): void {
     this.cartService.deleteCartItem(cartItemNo);
+    this.navigateToProductsIfCartIsEmpty();
+  }
+
+  private navigateToProductsIfCartIsEmpty(): void {
+    if (!this.cartDto || this.cartDto.items.length === 0) {
+      this.router.navigate(['/products']);
+    }
   }
 }
