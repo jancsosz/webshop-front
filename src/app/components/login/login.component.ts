8621561fd066cca2cb5bec79d47e61aa8f7d33ca
Janos Geszteredi <geszteredi.janika98@gmail.com>
2022-05-26 23:39:29 +0200

    implemented login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {TokenService} from '../../services/token.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  username = 'test';
+  password = '12345';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: AuthService,
+    private tokenService: TokenService
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  login(): void {
+    this.loginService.loginPOST(this.loginForm.get('username').value, this.loginForm.get('password').value)
+      .then(response => {
+        console.log('succ', response);
+      })
+      .catch(error => this.tokenService.setAccessToken(error.headers.get('Authorization')));
+  }
+
+  private createForm(): void {
+    this.loginForm = this.formBuilder.group({
+      username: [this.username, Validators.required],
+      password: [this.password, Validators.required]
+    });
+  }
 }
